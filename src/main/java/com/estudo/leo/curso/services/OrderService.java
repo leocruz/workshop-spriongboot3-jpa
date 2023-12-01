02a460aa164cd10d9138821e9abf8703367c1eef
Leonardo Dela Flora Cruz <leonardo.cruz@bbts.com.br>
2023-12-01 18:50:35 -0300

    Order, Instant, ISO 8601 - Parte 2

---
+++ src/main/java/com/estudo/leo/curso/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.estudo.leo.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudo.leo.curso.entities.Order;
+import com.estudo.leo.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll(){
+		return repository.findAll();	
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
