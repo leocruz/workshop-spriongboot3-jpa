a79aca573d2b033181aee3b53c625e6ad9b9b89f
Leonardo Dela Flora Cruz <leonardo.cruz@bbts.com.br>
2023-12-14 16:38:04 -0300

    Exception handling - findById

--- src/main/java/com/estudo/leo/curso/services/UserService.java
+++ src/main/java/com/estudo/leo/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.estudo.leo.curso.entities.User;
 import com.estudo.leo.curso.repositories.UserRepository;
+import com.estudo.leo.curso.services.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
